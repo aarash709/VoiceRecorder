0072420c2c099460c5a075cd0b377d8deff2b94a
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-06-25 14:35:22 +0330

    Add new colors.

--- core/designsystem/src/main/java/com/recorder/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/recorder/core/designsystem/theme/Theme.kt
@@ -1,40 +1,53 @@
 package com.recorder.core.designsystem.theme
 
+import android.content.res.Configuration
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200,
-    background = grayDarkBackground ,
-//    onBackground = Color.Black,
+    background = dark90,
+    onBackground = Color.LightGray,
+    surface = darkGraySurface,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onSurface = gray10,
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200,
-    background = grayLightBackground
-//    onBackground = Color.Black,
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    background = white,
+    onBackground = Color.Black,
+    surface = lightGraySurface,
     onPrimary = Color.White,
     onSecondary = Color.Black,
-    onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun VoiceRecorderTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable() () -> Unit,
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
@@ -48,4 +61,23 @@ fun VoiceRecorderTheme(
         shapes = Shapes,
         content = content
     )
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun ThemePreview() {
+    VoiceRecorderTheme {
+        Surface(modifier = Modifier) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.PlayCircleOutline, contentDescription = null)
+                }
+            }
+        }
+    }
 }
