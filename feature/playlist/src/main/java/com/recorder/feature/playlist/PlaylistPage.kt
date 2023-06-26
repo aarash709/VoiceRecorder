6a86d47db4c9763d1c111e117f85046daaa84b76
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-06-26 12:20:07 +0330

    Remove bottom sheet scaffold.

--- feature/playlist/src/main/java/com/recorder/feature/playlist/PlaylistPage.kt
+++ feature/playlist/src/main/java/com/recorder/feature/playlist/PlaylistPage.kt
@@ -5,14 +5,12 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.PlayCircle
-import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.material.icons.filled.StopCircle
 import androidx.compose.runtime.Composable
@@ -21,7 +19,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +27,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.core.common.model.Voice
 import com.recorder.core.designsystem.theme.VoiceRecorderTheme
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 @ExperimentalMaterialApi
@@ -40,55 +36,20 @@ fun Playlist(
     onStop: () -> Unit,
     onVoiceClicked: (Int, Voice) -> Unit,
 ) {
-
     val viewModel = hiltViewModel<PlaylistViewModel>()
     val voices = viewModel.voices.collectAsStateWithLifecycle().value
-
-    val bottomSheetState = rememberBottomSheetScaffoldState()
-    val scope = rememberCoroutineScope()
-    var index by remember {
+    var currentVoiceIndex by remember {
         mutableStateOf(0)
     }
-    LaunchedEffect(key1 = voices) {
-        if (!voices[index].isPlaying)
-            bottomSheetState.bottomSheetState.collapse()
-    }
-    LaunchedEffect(key1 = true) {
-        //load voices once the page is composed
-    }
-    BottomSheetScaffold(
-        sheetContent =
-        {
-            MediaControls(
-                voice = (voices[index]),
-                onPlayPause = { voice->
-                    viewModel.onPlay(index, voice) },
-                onStop = {
-                    viewModel.onStop()
-                    scope.launch {
-                        bottomSheetState.bottomSheetState.collapse()
-                    }
-                }
-            )
-        },
-        scaffoldState = bottomSheetState,
-        sheetShape = RoundedCornerShape(8.dp),
-        sheetPeekHeight = 0.dp,
-        backgroundColor = MaterialTheme.colors.background
-    ) {
-        PlaylistContent(
-            voices = voices,
-            isPlaying = false,
-            onPlayPause = { },
-            onStop = { },
-            onVoiceClicked = { i, voice ->
-                index = i
-                viewModel.onPlay(0,voice)
-                scope.launch {
-                    bottomSheetState.bottomSheetState.expand()
-                }
-            })
-    }
+    PlaylistContent(
+        voices = voices,
+        isPlaying = false,
+        onPlayPause = { },
+        onStop = { },
+        onVoiceClicked = { voiceIndex, voice ->
+            currentVoiceIndex = voiceIndex
+            viewModel.onPlay(0, voice)
+        })
 }
 
 @Composable
@@ -142,7 +103,7 @@ fun PlaylistItem(
     onVoiceClicked: (Voice) -> Unit,
 ) {
     val textColor = if (voice.isPlaying) MaterialTheme.colors.primary
-     else MaterialTheme.colors.onSurface
+    else MaterialTheme.colors.onSurface
     Surface(
         modifier = Modifier,
         shape = RoundedCornerShape(16.dp),
@@ -241,19 +202,20 @@ fun MediaControls(
 
     }
 }
+
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Composable
-fun PlaylistPagePreview(){
+fun PlaylistPagePreview() {
     VoiceRecorderTheme {
         Surface() {
             PlaylistContent(
                 listOf(
-                    Voice("title", "", isPlaying = false, "00:01",),
-                    Voice("title2", "", isPlaying = true, "00:10",),
-                    Voice("title3", "", isPlaying = false, "02:21",),
-                    Voice("title4", "", isPlaying = false, "05:01",),
-                    Voice("title5", "", isPlaying = false, "00:41",)
+                    Voice("title", "", isPlaying = false, "00:01"),
+                    Voice("title2", "", isPlaying = true, "00:10"),
+                    Voice("title3", "", isPlaying = false, "02:21"),
+                    Voice("title4", "", isPlaying = false, "05:01"),
+                    Voice("title5", "", isPlaying = false, "00:41")
                 ),
                 isPlaying = false,
                 onPlayPause = {},
@@ -264,11 +226,12 @@ fun PlaylistPagePreview(){
         }
     }
 }
+
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun PlaylistItemPreview() {
-    VoiceRecorderTheme{
+    VoiceRecorderTheme {
         Surface {
             PlaylistItem(
                 voice = Voice(
@@ -287,7 +250,7 @@ fun PlaylistItemPreview() {
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun MediaControlsPreview() {
-    VoiceRecorderTheme{
+    VoiceRecorderTheme {
         MediaControls(
             modifier = Modifier,
             voice = Voice(),
