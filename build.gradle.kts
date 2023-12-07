c282e2b2c1e54ec3e37f291865ca84ec89eb7e92
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-12-07 16:08:36 +0330

    Adding detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.tools.r8.internal.md
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,12 +7,38 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.kotlinter) apply false
 }
 
 buildscript {
     repositories {
         google()
         mavenCentral()
+    }
+}
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    tasks {
+        register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
+
+            exclude("**/resources/**")
+            exclude("**/build/**")
+            config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+            buildUponDefaultConfig = true
+            parallel = true
+//            allRules = true
+//            autoCorrect = true
+        }
+    }
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        reports {
+            html.required.set(true)
+            xml.required.set(false)
+            txt.required.set(false)
+            sarif.required.set(false)
+            md.required.set(false)
+        }
     }
 }
 
