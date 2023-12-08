b16e9f5da755470b3de7f2639d852969af2e5db4
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2023-12-08 14:23:15 +0330

    Add kotlinter plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import org.jmailen.gradle.kotlinter.tasks.LintTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library)  apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
@@ -15,8 +18,10 @@ buildscript {
         mavenCentral()
     }
 }
+
 subprojects {
     apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jmailen.kotlinter")
     tasks {
         register("detektAll", io.gitlab.arturbosch.detekt.Detekt::class) {
             exclude("**/resources/**")
@@ -28,17 +33,21 @@ subprojects {
 //            autoCorrect = true
         }
     }
+
     tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
         parallel = true
         //reports are set in config file
-//        reports {
-//            html.required.set(true)
-//            xml.required.set(false)
-//            txt.required.set(false)
-//            sarif.required.set(false)
-//            md.required.set(false)
-//        }
     }
+
+    tasks.withType<LintTask> {
+        ignoreFailures = true
+        source(files("src"))
+    }
+
+    tasks.withType<FormatTask> {
+
+    }
+
 }
 
 task("clean", Delete::class) {
