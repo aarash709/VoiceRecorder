6fc356bffea8959d44ca0f05b1775ccb55e38f16
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-01-01 16:12:14 +0330

    added feedback to recorder button when recording stopped

--- feature/record/src/main/java/com/recorder/feature/record/Record.kt
+++ feature/record/src/main/java/com/recorder/feature/record/Record.kt
@@ -2,6 +2,11 @@ package com.recorder.feature.record
 
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -25,6 +30,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -149,6 +156,24 @@ fun RecordAudioButton(
     isRecording: Boolean,
     startRecording: () -> Unit,
 ) {
+    val circleColor = MaterialTheme.colorScheme.primary
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val circleEffectRadius by infiniteTransition.animateFloat(
+        initialValue = 185f,
+        targetValue = 250f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, delayMillis = 150),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+    val circleAlpha by infiniteTransition.animateFloat(
+        initialValue = 0.8f,
+        targetValue = 0.0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, delayMillis = 150),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
@@ -158,9 +183,21 @@ fun RecordAudioButton(
             onClick = {
                 startRecording()
             },
-            border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
+            border = BorderStroke(3.dp, MaterialTheme.colorScheme.primary),
             enabled = recordingAllowed,
-            modifier = Modifier.size(125.dp),
+            modifier = Modifier
+                .size(125.dp)
+                .drawBehind {
+//                    initial = (size.minDimension / 2)
+                    drawCircle(
+                        color = circleColor,
+                        alpha = if (!isRecording) circleAlpha else 0f,
+                        radius = if (!isRecording) circleEffectRadius else size.minDimension / 2.0f,
+                        style = Stroke(
+                            width = 2.dp.toPx(),
+                        ),
+                    )
+                },
             shape = CircleShape
         ) {
             Icon(
