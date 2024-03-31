47d2cdeb5d2c15a08ada63dbefd5c7a0ff7df8f8
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-03-31 21:17:43 +0330

    removed record page from navigation graph

--- feature/playlist/src/main/kotlin/com/recorder/feature/playlist/PlaylistPage.kt
+++ feature/playlist/src/main/kotlin/com/recorder/feature/playlist/PlaylistPage.kt
@@ -10,12 +10,21 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardVoice
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -34,6 +43,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
@@ -214,13 +224,31 @@ fun PlaylistContent(
             )
         },
         bottomBar = {
-            PlaylistBottomBar(
-                isInEditMode = isInEditMode,
-                showRenameButton = showRenameButton,
-                selectedVoices = selectedVoices,
-                onShowRenameSheet = { showRenameSheet = it },
-                renameTextFieldValue = { renameTextFieldValue = it },
-                onDeleteVoices = { onDeleteVoices(it) })
+            BottomAppBar(
+                actions = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.KeyboardVoice,
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .clickable { /* TODO: Start recoding */ }
+                                .size(50.dp),
+                            contentDescription = "Recorder icon"
+                        )
+                    }
+                },
+                tonalElevation = 0.dp
+            )
+//            PlaylistBottomBar(
+//                isInEditMode = isInEditMode,
+//                showRenameButton = showRenameButton,
+//                selectedVoices = selectedVoices,
+//                onShowRenameSheet = { showRenameSheet = it },
+//                renameTextFieldValue = { renameTextFieldValue = it },
+//                onDeleteVoices = { onDeleteVoices(it) })
         }
     ) { paddingValues ->
         Column(
