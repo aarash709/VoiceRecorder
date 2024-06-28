5610f9e7d48ddabf076987f1a13da71fc415b860
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-06-28 22:05:43 +0330

    slider speed steps

--- feature/playlist/src/main/kotlin/com/recorder/feature/playlist/components/OptionsSheet.kt
+++ feature/playlist/src/main/kotlin/com/recorder/feature/playlist/components/OptionsSheet.kt
@@ -3,11 +3,12 @@ package com.recorder.feature.playlist.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -22,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -33,13 +35,13 @@ import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OptionsSheet(modifier: Modifier = Modifier,onDismissRequest:()->Unit) {
+fun OptionsSheet(modifier: Modifier = Modifier, onDismissRequest: () -> Unit) {
     ModalBottomSheet(onDismissRequest = { onDismissRequest() }) {
         Surface(modifier = modifier.fillMaxWidth()) {
             Column(
                 Modifier
                     .fillMaxWidth()
-                    .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(horizontal = 16.dp), horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
@@ -51,9 +53,9 @@ fun OptionsSheet(modifier: Modifier = Modifier,onDismissRequest:()->Unit) {
                         colors = ButtonDefaults
                             .textButtonColors(contentColor = MaterialTheme.colorScheme.onBackground)
                     ) {
-                        Text(text = "Reset")
+                        Text(text = "Reset", style = MaterialTheme.typography.titleMedium)
                     }
-                    Text(text = "Options")
+                    Text(text = "Options", style = MaterialTheme.typography.titleMedium)
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(
                             imageVector = Icons.Default.Close,
@@ -61,23 +63,36 @@ fun OptionsSheet(modifier: Modifier = Modifier,onDismissRequest:()->Unit) {
                         )
                     }
                 }
-                Column {
-                    Text(text = "Playback speed", fontSize = 14.sp)
-                    Row {
-                        Slider(value = 1f, onValueChange = {})
+                Spacer(modifier = Modifier.height(16.dp))
+                Column(modifier = Modifier.padding(horizontal = 12.dp)) {
+                    Column {
+                        Text(text = "Playback speed", fontSize = 14.sp)
+                        Row(modifier = Modifier.padding(horizontal = 0.dp), verticalAlignment = Alignment.CenterVertically) {
+                            var value by remember {
+                                mutableFloatStateOf(1f)
+                            }
+                            Slider(
+                                value = value,
+                                onValueChange = { value = it },
+                                steps = 2,
+                                valueRange = 0.5f..2.0f
+                            )
+                        }
                     }
-                }
-                HorizontalDivider()
-                Row(
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    var checked by remember {
-                        mutableStateOf(false)
+                    HorizontalDivider()
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        var checked by remember {
+                            mutableStateOf(false)
+                        }
+                        Text(text = "Skip Silence", fontSize = 16.sp)
+                        Switch(checked = checked, onCheckedChange = { checked = it })
                     }
-                    Text(text = "Skip Silence", fontSize = 14.sp)
-                    Switch(checked = checked, onCheckedChange = { checked = it })
                 }
             }
         }
