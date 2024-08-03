a046636873895d24149cd18fda8a128bdab2f213
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-08-03 18:21:54 +0330

    cleanup

--- feature/record/src/main/kotlin/com/recorder/feature/record/Navigation.kt
+++ feature/record/src/main/kotlin/com/recorder/feature/record/Navigation.kt
@@ -1,13 +1,9 @@
 package com.recorder.feature.record
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
-import androidx.compose.animation.shrinkOut
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
@@ -26,20 +22,9 @@ fun NavGraphBuilder.recorder(
         route = RECORDER_ROUTE,
         enterTransition = {
             fadeIn(initialAlpha = 0.5f) + scaleIn(initialScale = 0.9f)
-//            slideIntoContainer(
-//                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-//                    animationSpec = tween(350),
-//                    initialOffset = { it/3 }
-//                )
         },
         exitTransition = {
-            fadeOut(targetAlpha = 0f) + scaleOut(targetScale = 0.9f)            /*when (targetState.destination.route) {
-                else -> slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = tween(350),
-                    targetOffset = { (it/3) }
-                )
-            }*/
+            fadeOut(targetAlpha = 0f) + scaleOut(targetScale = 0.9f)
         }){
         Record(
             onNavigateToPlaylist = { onNavigateToPlaylist() }
