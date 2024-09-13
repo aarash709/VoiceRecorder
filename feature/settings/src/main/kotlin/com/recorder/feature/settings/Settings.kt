4e6a51c4d49257e83b98b297ebc6b093e20fbf5f
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-09-13 16:53:59 +0330

    fix nested scroll

--- feature/settings/src/main/kotlin/com/recorder/feature/settings/Settings.kt
+++ feature/settings/src/main/kotlin/com/recorder/feature/settings/Settings.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -97,6 +100,8 @@ fun SettingsContent(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
+                    .nestedScroll(scrollBehavior.nestedScrollConnection)
+                    .verticalScroll(rememberScrollState())
             ) {
                 SettingsItemWithSwitch(
                     title = stringResource(id = R.string.earpiece_mode),
