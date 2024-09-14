c8dc765df6184d5ecc14924c225dadfd76ebc7e8
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-09-14 17:47:53 +0330

    dismiss dialog on options change

--- feature/settings/src/main/kotlin/com/recorder/feature/settings/SettingsItem.kt
+++ feature/settings/src/main/kotlin/com/recorder/feature/settings/SettingsItem.kt
@@ -2,7 +2,6 @@ package com.recorder.feature.settings
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,17 +13,16 @@ import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.UnfoldMore
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -82,6 +80,9 @@ internal fun SettingsItemWithOptions(
 ) {
     var shouldShowOptions by remember {
         mutableStateOf(false)
+    }
+    LaunchedEffect(currentActiveOption) {
+        shouldShowOptions = false
     }
     Surface(
         modifier = modifier
