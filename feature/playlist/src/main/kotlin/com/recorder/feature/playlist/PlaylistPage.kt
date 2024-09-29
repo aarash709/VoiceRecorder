3d3eab6e2e8c9a9e62eaf48bc97ac4c4bc085681
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-09-29 18:10:08 +0330

    added placeholder sorting chips

--- feature/playlist/src/main/kotlin/com/recorder/feature/playlist/PlaylistPage.kt
+++ feature/playlist/src/main/kotlin/com/recorder/feature/playlist/PlaylistPage.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -23,11 +25,15 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -316,6 +322,27 @@ fun PlaylistContent(
 							selectedVoices = emptySet()
 						}
 
+					)
+				}
+				var selectChip by remember { mutableStateOf(false) }
+				Row(
+					modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+					horizontalArrangement = Arrangement.spacedBy(16.dp)
+				) {
+					FilterChip(
+						selected = selectChip,
+						onClick = { selectChip = !selectChip },
+						label = { Text("Date") }, border = BorderStroke(0.dp, Color.Transparent)
+					)
+					FilterChip(
+						selected = selectChip,
+						onClick = { selectChip = !selectChip },
+						label = { Text("Name") }, border = BorderStroke(0.dp, Color.Transparent)
+					)
+					FilterChip(
+						selected = selectChip,
+						onClick = { selectChip = !selectChip },
+						label = { Text("Duration") }, border = BorderStroke(0.dp, Color.Transparent)
 					)
 				}
 				if (voices.isEmpty()) {
