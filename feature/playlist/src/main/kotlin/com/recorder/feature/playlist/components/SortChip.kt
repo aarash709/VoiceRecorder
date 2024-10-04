bfcfe4b2b5d5527f5643b4637643a941f3b61f30
Arash <68777571+arashebrahimzade@users.noreply.github.com>
2024-10-04 14:15:50 +0330

    change duration `check` icon to `clear`

--- feature/playlist/src/main/kotlin/com/recorder/feature/playlist/components/SortChip.kt
+++ feature/playlist/src/main/kotlin/com/recorder/feature/playlist/components/SortChip.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FilterChip
@@ -96,7 +97,7 @@ internal fun DurationSortChip(
 				leadingIcon = {
 					AnimatedVisibility(sortedByDuration.isSelected) {
 						Icon(
-							Icons.Default.Check,
+							Icons.Default.Clear,
 							modifier = Modifier.clickable {
 								onSetByDurationChange(
 									SortByDuration(
