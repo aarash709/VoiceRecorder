c2a9efe71e51c9c1e263d90baee3f7aed8b1908b
Arash Ebrahimzade <68777571+aarash709@users.noreply.github.com>
2024-12-07 22:25:28 +0330

    remove kotlinter
    fixes compiler error after upgrading to Kotlin 2.1.0 with Kotlinter plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.kotlinter) apply false
+//  alias(libs.plugins.kotlinter) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlinx.ksp) apply false
 }
